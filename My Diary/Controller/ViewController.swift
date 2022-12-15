18fb5155bdbb691bb1baa1636b589f0ef7435636
mohammed alsaad <mohaalsaad.j@gmail.com>
2022-12-15 17:03:53 +0300

    V 2.2 ,, login and sing up view

--- My Diary/Controller/ViewController.swift
+++ My Diary/Controller/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   //MARK: - IBOutlets
+    
+    @IBOutlet weak var userNameTextFilde: UITextField!
+    @IBOutlet weak var passwordTextFilde: UITextField!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func singUpButton(_ sender: Any) {
+    }
+   
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    
+    
 }
 
---
+++ My Diary/Controller/singUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  singUpViewController.swift
+//  My Diary
+//
+//  Created by mohammed alsaad on 15/12/2022.
+//
+
+import UIKit
+
+class singUpViewController: UIViewController {
+
+    
+  //MARK: - IBOutlet
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var reWritePasswordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+    
+
+    
+     
+}
