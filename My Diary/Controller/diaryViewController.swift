d2816840379f9099a154daaf67ff83374f0dda16
Shaden <shadenaljalo@gmail.com>
2022-12-15 22:42:31 -0500

    V 3.0 adding side menu for diary screen

--- My Diary/Controller/diaryViewController.swift
+++ My Diary/Controller/diaryViewController.swift
@@ -10,12 +10,43 @@ import UIKit
 class diaryViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var backgroundView: UIView!
     
+    @IBOutlet weak var trailingMenuConstant: NSLayoutConstraint!
+    
+    var menuVC: menuViewController?
     override func viewDidLoad() {
         super.viewDidLoad()
             collectionView.delegate = self
             collectionView.dataSource = self
+        
+            backgroundView.isHidden = true
         }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "sideMenu")
+        {
+            if let controller = segue.destination as? menuViewController
+            {
+                self.menuVC = controller
+                self.menuVC?.menuDelegate = self
+            }
+        }
+    }
+    
+    private func hiddeMenuView(){
+        self.backgroundView.isHidden = true
+        self.trailingMenuConstant.constant = 190
+        
+    }
+    
+    @IBAction func presentMenu(_ sender: UIButton){
+        backgroundView.isHidden = false
+        trailingMenuConstant.constant = 0
+    }
+    
+    
     
    
 
@@ -37,6 +68,14 @@ extension diaryViewController: UICollectionViewDelegate, UICollectionViewDataSou
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: self.view.frame.width * 0.8, height: self.view.frame.height * 0.12)
 
+    }
+    
+}
+
+extension diaryViewController: Menu{
+    func hiddeMenu(){
+        self.hiddeMenuView()
+        
     }
     
 }
