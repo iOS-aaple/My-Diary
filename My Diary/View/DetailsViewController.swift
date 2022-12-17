95b2f015fe6bf53cc6ed5d568ff1d340706f290d
safcsp <mnorybh@gmail.com>
2022-12-17 15:09:58 +0300

    V 6.0

--- My Diary/View/DetailsViewController.swift
+++ My Diary/View/DetailsViewController.swift
@@ -15,9 +15,37 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        backgroundView.layer.cornerRadius = 40
+        setUpUI()
+       
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        animateView()
     }
     
-
+    // MARK: - set UI
+    func setUpUI(){
+        backgroundView.alpha = 0
+        backgroundView.layer.cornerRadius = 40
+    }
+    //MARK: - animate view
+    func animateView(){
+        UIView.animate(withDuration: 1.5) {
+            self.backgroundView.alpha = 1
+        }
+        UIView.animate(withDuration: 1) {
+            self.backgroundView.center.y -= 50
+        } completion: { _ in
+            
+        }
+    }
+    
+    
+    
+    @IBAction func editButton(_ sender: UIButton) {
+        let storyBoard = UIStoryboard(name: "AddEdit", bundle: nil)
+        let editDetails = storyBoard.instantiateViewController(withIdentifier: "AddEdit")
+        editDetails.modalPresentationStyle = .fullScreen
+        present(editDetails, animated: true)
+    }
+    
 }
